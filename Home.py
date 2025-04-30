6414b2e289d8b8e4c63ad5fe0291a86a5a86682b
Aimar Anshari <aiimaransharii@gmail.com>
2025-04-30 23:20:25 +0800

    Update and rename streamlit_app.py to Home.py

--- Home.py
+++ Home.py
@@ -3,11 +3,6 @@ from keras.models import load_model
 from PIL import ImageOps, Image
 from util import classify
 
-st.set_page_config(
-    page_title="Main"
-)
-
-
 st.title(":green[AI Image Investigator] :mag_right:")
 
 uploaded_files = st.file_uploader(
