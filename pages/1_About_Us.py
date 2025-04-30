9ae6538a561a496cd6d0db267f5f31bff71b37a6
Aimar Anshari <aiimaransharii@gmail.com>
2025-04-30 23:07:05 +0800

    Create 1_About_Us.py

---
+++ pages/1_About_Us.py
@@ -0,0 +1,25 @@
+import base64
+import streamlit as st
+from PIL import Image, ImageOps
+import numpy as np
+
+def classify(image, model, class_names):
+    
+    image = ImageOps.fit(image, (128, 128), method = Image.Resampling.LANCZOS)
+    
+    image_array = np.asarray(image)
+    
+    normalized_image_array = (image_array.astype(np.float32) / 127.5) - 1
+    
+    data = np.ndarray(shape=(1, 128, 128, 3), dtype=np.float32)
+    data[0] = normalized_image_array
+    
+    prediction = model.predict(data)
+    index = 0 if prediction[0][0] > 0.95 else 1
+    class_name = class_names[index]
+    confidence_score = prediction[0][index]
+    
+    return class_name, confidence_score
+
+
+    
