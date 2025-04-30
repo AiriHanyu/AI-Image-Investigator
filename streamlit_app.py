c4a2c39cd4f8e2f4f997dcbafb70924d1b570612
Aimar Anshari <aiimaransharii@gmail.com>
2025-04-30 12:07:04 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,49 +19,6 @@ with open('label.txt', 'r') as f:
     class_names = [a[:-1].split(' ')[1] for a in f.readlines()]
     f.close()
 
-def predict_image(img_pil):
-    img_resized = img_pil.resize((128, 128))
-    img_array = img_to_array(img_resized) / 255.0
-    img_array = np.expand_dims(img_array, axis=0)
-    prediction = model.predict(img_array)
-    return np.argmax(prediction)
-
-if uploaded_files:
-    images_to_check = []
-    for uploaded_file in uploaded_files:
-        image = Image.open(uploaded_file).convert("RGB")  # Convert biar aman
-        st.image(image, caption=uploaded_file.name, use_container_width=True)
-        images_to_check.append((uploaded_file.name, image))
-
-    st.success(f"{len(uploaded_files)} gambar berhasil diunggah!")
-
-if st.button("Investigasi"):
-        st.subheader("Hasil Investigasi")
-        for name, img in images_to_check:
-            label_idx = predict_image(img)
-            pred_label = class_names[label_idx]  # pakai dari label.txt
-
-            color = (0, 255, 0) if pred_label.upper() == "REAL" else (255, 0, 0)
-
-            img_with_text = img.copy()
-            draw = ImageDraw.Draw(img_with_text)
-
-            font_size = int(img_with_text.width * 0.1)
-            font_path = "/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf"
-            font = ImageFont.truetype(font_path, font_size)
-
-            text = pred_label
-            text_bbox = draw.textbbox((0, 0), text, font=font)
-            text_width, text_height = text_bbox[2], text_bbox[3]
-            pos = ((img.width - text_width) // 2, (img.height - text_height) // 2)
-
-            text_img = Image.new("RGBA", img.size, (255, 255, 255, 0))
-            text_draw = ImageDraw.Draw(text_img)
-            text_draw.text(pos, text, fill=color, font=font)
-
-            text_img_rotated = text_img.rotate(45, resample=Image.BICUBIC, expand=True)
-            rotated_pos = ((img.width - text_img_rotated.width) // 2,
-                           (img.height - text_img_rotated.height) // 2)
-
-            img_with_text.paste(text_img_rotated, rotated_pos, mask=text_img_rotated)
-            st.image(img_with_text, caption=f"{name} - {pred_label}", use_container_width=True)
+if file is not None:
+    image = Image.open(uploaded_files).convert('RGB')
+    st.image(image, use_column_width = True)
