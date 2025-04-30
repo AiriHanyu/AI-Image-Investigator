a407a59622b823f1e86eb3f97f8a5d4d3c7404a7
Aimar Anshari <aiimaransharii@gmail.com>
2025-05-01 01:41:44 +0800

    Update 1_About_Us.py

--- pages/1_About_Us.py
+++ pages/1_About_Us.py
@@ -5,9 +5,11 @@ st.title("About Us")
 st.markdown("""
 ### AIMAR ANSHARI  
 [Instagram](https://instagram.com/aynshz_ryuxzy)
+[Email](aiimaransharii@gmail.com)
 
 ---
 
 ### DEWI AURA SYALSABILLAH F  
 [Instagram](https://instagram.com/quivyy_)
+[Email](fadjerisyalsabillah@gmail.com)
 """)
